617f620d23a49d86ca90a6d5adff9691f78fa166
Josh <joshmerickson19@gmail.com>
2023-05-04 14:42:51 -0700

    Formatting and finalizing changes

--- interfaceWIP.py
+++ interfaceWIP.py
@@ -54,10 +54,7 @@ class HomePage(ctk.CTkFrame):
         self.controller.title('Dupligone!')
         self.controller.state('zoomed')
 
-        heading_label = ctk.CTkLabel(self,
-                                                     text='Dupligone!',
-                                                     font=('Roboto',45,'bold'),
-                                                     )
+        heading_label = ctk.CTkLabel(self, text='Dupligone!', font=('Roboto',45,'bold'))
         heading_label.pack(pady=25)
 
         # Home Screen Description 1
@@ -89,7 +86,7 @@ class HomePage(ctk.CTkFrame):
 
 # Page for browsing folders and uploading files to workspace        
 class UploadPage(ctk.CTkFrame):
-    # end goal: page split into two sections (boxes). Left box contains browse folder button, right box contains preview of uploaded filepaths
+    # Page split into two sections (boxes). Left box contains browse folder button, right box contains preview of uploaded filepaths
         # may need to implement function that shows browse folders
 
     def __init__(self, parent, controller):
@@ -97,16 +94,11 @@ class UploadPage(ctk.CTkFrame):
         self.controller = controller
 
         # Header Label for Dupligone Logo
-        heading_label = ctk.CTkLabel(self,
-                                     text='Dupligone!',
-                                     font=('Roboto',45,'bold'))
+        heading_label = ctk.CTkLabel(self, text='Dupligone!', font=('Roboto',45,'bold'))
         heading_label.pack(pady=25)
         # Sub Header Label for page description
-        main_menu_label = ctk.CTkLabel(self,
-                                       text='Upload Content to Workspace',
-                                       font=('Roboto',20))
+        main_menu_label = ctk.CTkLabel(self, text='Upload Content to Workspace', font=('Roboto',20))
         main_menu_label.pack()
-
 
         # Frame for left box
         left_frame = ctk.CTkFrame(self,border_width=3)
@@ -123,10 +115,7 @@ class UploadPage(ctk.CTkFrame):
                 right_frame.insert("end", "\n")
             right_frame.configure(state="disabled") # enable editing state of text box
 
-        search_button = ctk.CTkButton(left_frame,
-                                      text='Browse',
-                                      font=('Roboto',20),
-                                      command=pressedBrowse)
+        search_button = ctk.CTkButton(left_frame, text='Browse', font=('Roboto',20), command=pressedBrowse)
         search_button.pack(side='bottom', pady=25)
 
         # Textbox Frame for right box
@@ -138,10 +127,7 @@ class UploadPage(ctk.CTkFrame):
             controller.show_frame('HomePage')
             print("Exit button pressed, returning to homepage")
         # Create Exit button    
-        exit_button = ctk.CTkButton(self,
-                                    text='Exit',
-                                    font=('Roboto',15),
-                                    command=exit)
+        exit_button = ctk.CTkButton(self, text='Exit', font=('Roboto',15), command=exit)
         exit_button.pack(side='bottom', pady=10)
         
         # Function for Search button to progress to search results page
@@ -149,11 +135,7 @@ class UploadPage(ctk.CTkFrame):
             controller.show_frame('SearchPage')
             print("Search button pressed, moving to SearchPage")
         # Create search page button
-        goto_search_button = ctk.CTkButton(self,
-                                    text='Begin Search',
-                                    font=('Roboto',30),
-                                    command=gotoSearch
-                                    )
+        goto_search_button = ctk.CTkButton(self, text='Begin Search', font=('Roboto',30), command=gotoSearch)
         goto_search_button.pack(side='bottom', pady=10, ipadx=2, ipady=2)
 
         
@@ -166,22 +148,16 @@ class SearchPage(ctk.CTkFrame):
         self.controller = controller
 
         # Header Label for Dupligone Logo
-        heading_label = ctk.CTkLabel(self,
-                                     text='Dupligone!',
-                                     font=('Roboto',45,'bold'))
+        heading_label = ctk.CTkLabel(self, text='Dupligone!', font=('Roboto',45,'bold'))
         heading_label.pack(pady=25)
 
         # header frame for top labels
         header_frame = ctk.CTkFrame(self,fg_color="transparent")
         header_frame.pack(fill="x", side='top', padx=5, pady=5)
 
-        duplicates_label = ctk.CTkLabel(header_frame,
-                                       text='Duplicates',
-                                       font=('Roboto',22, 'bold'))
+        duplicates_label = ctk.CTkLabel(header_frame, text='Duplicates', font=('Roboto',22, 'bold'))
         duplicates_label.pack(fill="x",side='left',padx=200)
-        image_preview_label = ctk.CTkLabel(header_frame,
-                                       text='Image Preview',
-                                       font=('Roboto',22, 'bold'))
+        image_preview_label = ctk.CTkLabel(header_frame, text='Image Preview', font=('Roboto',22, 'bold'))
         image_preview_label.pack(fill="x",side='right',padx=200)
         
         # Frame for bottom buttons box
@@ -201,19 +177,19 @@ class SearchPage(ctk.CTkFrame):
             for key, value in found_images_dict.items():
                 print(key, " : ", value)
 
-            test_output.configure(state="normal") # enable editing state of text box
+            #test_output.configure(state="normal") # enable editing state of text box
             for value in found_filepaths_dict:
                 # Test Skeletal Code for displaying the images
                 # current_image = ctk.CTkImage(Image.open(r"[filepath]"), size=(100,100))
                 print(found_filepaths_dict[value])
-                test_output.insert("end", found_filepaths_dict[value])
-                test_output.insert("end", "\n\n")
-            test_output.configure(state="disabled") # disable editing state after finished writing results
+                #test_output.insert("end", found_filepaths_dict[value])
+                #test_output.insert("end", "\n\n")
+            #test_output.configure(state="disabled") # disable editing state after finished writing results
             # to-fix note: if user exits page after text insertion, text remains when visiting the page again
 
         # Textbox for debugging display purposes
-        test_output = ctk.CTkTextbox(left_frame, state="disabled")
-        test_output.grid(row=0,column=0,columnspan=4,pady=5,padx=5)
+        #test_output = ctk.CTkTextbox(left_frame, state="disabled")
+        #test_output.grid(row=0,column=0,columnspan=4,pady=5,padx=5)
 
         # Function to call for image preview
         def show_in_preview(image, text):
@@ -256,7 +232,7 @@ class SearchPage(ctk.CTkFrame):
 
         # Function to call for image preview
         preview_image = ctk.CTkImage(Image.open(r"C:\Users\theun\OneDrive\Documents\Dupligone\TestImages\AC2.jpg"), size=(500,350))
-        preview_image_button = ctk.CTkButton(right_frame, image=preview_image, width=500, height=350, text='AC2.jpg', font=('Roboto',15), compound="top", fg_color="transparent", hover=True)
+        preview_image_button = ctk.CTkButton(right_frame, image=preview_image, width=500, height=350, text='AC2.jpg', font=('Roboto',15), compound="top", fg_color="transparent", hover=False)
         preview_image_button.grid(row=0,column=0,columnspan=4,pady=5,padx=5)
         
         # Function to delete image; tested with Image 5 D2.png
@@ -300,28 +276,24 @@ class SearchPage(ctk.CTkFrame):
                 self.geometry("1280x720")
 
                 # Header Label for Dupligone Logo
-                compare_head_label = ctk.CTkLabel(self,
-                                     text='Preview',
-                                     font=('Roboto',20,'bold'))
+                compare_head_label = ctk.CTkLabel(self, text='Preview', font=('Roboto',20,'bold'))
                 compare_head_label.pack(pady=25)
 
                 # Frame for Left Side (Duplicate Viewer Window)
                 left_compare_frame = ctk.CTkFrame(self, border_width=1, width=600, height=500)
                 left_compare_frame.pack(fill='both',side='left', expand=True, padx=10, pady=10)
 
-                left_image = ctk.CTkImage(Image.open(r"C:\Users\theun\OneDrive\Documents\Dupligone\TestImages\AC1.jpg"), size=(500,350))
-                left_image_button = ctk.CTkButton(left_compare_frame, image=left_image, width=500, height=350, text='AC1.jpg', font=('Roboto',15), compound="top", fg_color="transparent", hover=True)
+                left_image = ctk.CTkImage(Image.open(r"C:\Users\theun\OneDrive\Documents\Dupligone\TestImages\AC1.jpg"), size=(550,400))
+                left_image_button = ctk.CTkButton(left_compare_frame, image=left_image, width=550, height=400, text='AC1.jpg', font=('Roboto',15), compound="top", fg_color="transparent", hover=False)
                 left_image_button.grid(row=0,column=0,columnspan=4,pady=5,padx=5)
 
                 # Frame for right box (Image Preview Window)
                 right_compare_frame = ctk.CTkFrame(self,border_width=1, width=600, height=500)
                 right_compare_frame.pack(fill="both", side='left',  expand=True, padx=10, pady=10) 
 
-                right_image = ctk.CTkImage(Image.open(r"C:\Users\theun\OneDrive\Documents\Dupligone\TestImages\AC2.jpg"), size=(500,350))
-                right_image_button = ctk.CTkButton(right_compare_frame, image=right_image, width=500, height=350, text='AC2.jpg', font=('Roboto',15), compound="top", fg_color="transparent", hover=True)
+                right_image = ctk.CTkImage(Image.open(r"C:\Users\theun\OneDrive\Documents\Dupligone\TestImages\AC2.jpg"), size=(550,400))
+                right_image_button = ctk.CTkButton(right_compare_frame, image=right_image, width=550, height=400, text='AC2.jpg', font=('Roboto',15), compound="top", fg_color="transparent", hover=False)
                 right_image_button.grid(row=0,column=0,columnspan=4,pady=5,padx=5)
-                #self.entry = ctk.CTkEntry(self, placeholder_text="Enter new name")
-                #self.entry.pack(padx=20)
 
         # Function to open top level window for compare related
         def open_comparelevel(self):
@@ -338,21 +310,21 @@ class SearchPage(ctk.CTkFrame):
         delete_button.configure(command=lambda: delete_image(r"C:\Users\theun\OneDrive\Documents\Dupligone\TestImages\Subfolder2\D2.jpg")) # test to delete last image D2.png
         delete_button.grid(row=1,column=0,pady=5,padx=5)
         rename_button = ctk.CTkButton(right_frame, text='Rename', font=('Roboto',15))
-        rename_button.configure(command=lambda: open_renamelevel(self))
+        rename_button.configure(command=lambda: open_renamelevel(self)) # test with Kirby photo
         rename_button.grid(row=1,column=1,pady=5,padx=5)
         move_button = ctk.CTkButton(right_frame, text='Move', font=('Roboto',15))
         move_button.grid(row=1,column=2,pady=5,padx=5)
-        compare_button = ctk.CTkButton(right_frame, text='Compare Related', font=('Roboto',15), fg_color="#1338BE")
+        compare_button = ctk.CTkButton(right_frame, text='Compare Related', font=('Roboto',15), fg_color="#1338BE") # test with AC1 and AC2
         compare_button.configure(command=lambda: open_comparelevel(self))
         compare_button.grid(row=1,column=3,pady=5,padx=5)
 
         # Text button for search algo debugging
-        search_button = ctk.CTkButton(bottom_frame,
-                                      text='TestSearch',
-                                      font=('Roboto', 15),
-                                      command=testSearch)
+        #search_button = ctk.CTkButton(bottom_frame,
+                                      #text='TestSearch',
+                                      #font=('Roboto', 15),
+                                      #command=testSearch)
         #search_button.pack(side='bottom')
-        search_button.grid(row=0,column=0,pady=5,padx=5)
+        #search_button.grid(row=0,column=0,pady=5,padx=5)
 
         # Exit function to return to homepage
         def exit():
@@ -361,10 +333,7 @@ class SearchPage(ctk.CTkFrame):
             # to-do: prompt user if they are sure they wish to go to homepage, wipe any texts, images displayed from the page
             
         # Exit button 
-        exit_button = ctk.CTkButton(bottom_frame,
-                                    text='Exit',
-                                    font=('Roboto', 15),
-                                    command=exit)
+        exit_button = ctk.CTkButton(bottom_frame, text='Exit', font=('Roboto', 15), command=exit)
         exit_button.grid(row=0,column=1,pady=5,padx=5)
 
 if __name__ == "__main__":
